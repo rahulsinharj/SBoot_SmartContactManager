aee6c47d329bce5038f726ab675fa1a0f7d2255c
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2022-03-13 12:23:36 +0530

    First commit

---
+++ Sboot_SmartContactManager/src/main/java/smartmg/SbootSmartContactManagerApplication.java
@@ -0,0 +1,16 @@
+package smartmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootSmartContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootSmartContactManagerApplication.class, args);
+		
+		System.out.println("Welcome to SmartContactManager !");
+		
+	}
+
+}
---
+++ Sboot_SmartContactManager/src/test/java/smartmg/SbootSmartContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package smartmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootSmartContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
