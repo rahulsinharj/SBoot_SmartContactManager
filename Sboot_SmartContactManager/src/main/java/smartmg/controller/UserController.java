54aadb63f628313fab0e22c9607a1305e19bc458
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2022-03-22 17:47:23 +0530

    addcontact form link

--- Sboot_SmartContactManager/src/main/java/smartmg/controller/UserController.java
+++ Sboot_SmartContactManager/src/main/java/smartmg/controller/UserController.java
@@ -5,9 +5,12 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import smartmg.dao.UserRepository;
+import smartmg.entity.Contact;
 import smartmg.entity.User;
 
 @Controller
@@ -17,8 +20,9 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 	
-	@RequestMapping("/index")
-	public String dashboard(Model model, Principal principal)
+	// Method for adding common data to response
+	@ModelAttribute
+	public void addCommonData(Model model, Principal principal)
 	{
 		String loginUserName = principal.getName();				// Get the user(user email) through Principal obj
 		System.out.println("USER Email : "+ loginUserName);
@@ -28,10 +32,24 @@ public class UserController {
 		System.out.println("USER info : "+user);
 		
 		model.addAttribute(user);
-		
+	}
+	
+	// Dashboard home
+	@RequestMapping("/index")
+	public String dashboard()
+	{
 		return "normal/user_dashboard";				// that means template folder ke inside >> "normal" folder ke andar "user_dashboard.html" milega 
 	}
 	
-		
+	// Open Add Form Handler ::
+	@GetMapping("/add-contact")
+	public String openAddContactForm(Model model) 
+	{
+		model.addAttribute("contact",new Contact());
+		return "normal/add_contact_form";
+	}
+	
+	
+	
 	
 }
